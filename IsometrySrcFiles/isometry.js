bf714d7341d56a6c36d69e815073e3316c39d6be
Michael Kielbasinski <85803354+MiniKielbyM@users.noreply.github.com>
2023-09-26 12:16:43 +0000

    Pending changes exported from your codespace

--- IsometrySrcFiles/isometry.js
+++ IsometrySrcFiles/isometry.js
@@ -1,6 +1,6 @@
 class Isometry {
   constructor() {
-    alert('test1')
+
     var canvas = document.createElement('canvas');
     canvas.width = 320;
     canvas.height = 322;
@@ -11,7 +11,6 @@ class Isometry {
     img.src = 'https://github.com/MiniKielbyM/Isometry/blob/main/IsometrySrcFiles/Elements/base.png?raw=true';
     img.onload = function () {
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      alert('image loaded')
     };
     document.body.appendChild(canvas);
   }
@@ -25,15 +24,17 @@ class Isometry {
         ctx.drawImage(img, 144, 64);//0, 0
       }
     }
-    cube(x = 0, y = 0) {
+    async cube(x = 0, y = 0) {
       var canvasIso = document.getElementById('Iso_Engine_Canvas')
       var ctx = canvasIso.getContext('2d');
       var img = new Image();
       img.src = 'https://github.com/MiniKielbyM/Isometry/blob/main/IsometrySrcFiles/Elements/element_0.png?raw=true';
-      img.onload = function () {
-        ctx.drawImage(img, 144 - (Math.round(y) * 16) + (Math.round(x) * 16), 64 + (Math.round(y) * 8) + (Math.round(x) * 8));//0, 0
+      img.onload = async function () {
+        await ctx.drawImage(img, 144 - (Math.round(y) * 16) + (Math.round(x) * 16), 64 + (Math.round(y) * 8) + (Math.round(x) * 8));//0, 0
+        setTimeout(console.log("test"),5000)  
       }
 
     }
+
   };
 };
